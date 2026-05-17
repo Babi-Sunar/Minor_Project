c52da1542d2348a78b852d8286a7e25924238289
Babi-Sunar <babi.sunar3737@gmail.com>
2026-05-17 17:04:04 +0545

    home page completed

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ function toggleMenu(){
 
 // join-session
 function joinSession(){
-    let join_btn = document.getElementById("")
+    let join_btn = document.getElementById("");
+}
+
+//connect form submit
+function connect_form_submit(){
+    let form = document.getElementById("connect-form");
+    form.addEventListener('submit', function(event){
+        alert("🏆🎉Success! \n You've unlocked: 'Contact Form Completed' achievement. ");
+    })
 }
